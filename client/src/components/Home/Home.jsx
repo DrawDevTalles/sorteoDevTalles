aeff193708bc31a98984e8ade539d013fa534acf
Juliana <jflorez@proton.me>
2024-03-15 01:35:38 -0500

    borrador home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,19 @@
 function Home() {
-
+let sorteo = ""
+let fechaInicio = ""
+let fechaFin = ""
+let ganador = ""
     return (
-     <p> Este es el home</p>
+     <div>
+        <h1> El Sorteo actual es {sorteo}</h1>
+        <h2> Inició en : {fechaInicio} </h2>
+        <h2> Termina en: {fechaFin} </h2>
+        <h2> El ganador es: {ganador} </h2>
+        <h3> Si quieres participar debes registrarte:</h3> 
+        <button>Participa</button>
+
+
+     </div>
     )
   }
   
