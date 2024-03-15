3f0f8d2a8bf56292469892a8ce475c722449f2e3
gabrielaDF <defreitasgabi@gmail.com>
2024-03-15 11:34:07 -0500

    configuracion de talwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      darkBlue: "#1d1238",
+      white: "#fff",
+      purpleBlue1: "#251849",
+      purpleBlue2: "#26184A",
+      darkPurple: "#130c25",
+      purple: "#9013fe",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
