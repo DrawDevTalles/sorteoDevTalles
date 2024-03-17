eebc407d52e0cdc6e1aa47fb4a707ff33fb8da76
gabrielaDF <defreitasgabi@gmail.com>
2024-03-17 10:34:14 -0500

    actualizacion de ruta Home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Routes, Route, useLocation } from "react-router-dom";
 
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./Components/Home/Home";
 import "./App.css";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Register from "./components/Register/Register";
