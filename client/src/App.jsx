43c4be1b3ebf32efe7d8000fd66eee47a78f9567
gabrielaDF <defreitasgabi@gmail.com>
2024-03-17 10:44:15 -0500

    modificacion nombre carpetas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import { Routes, Route, useLocation } from "react-router-dom";
-import Home from "./Components/Home/Home";
+import Home from "./components/Home/Home";
 import "./App.css";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Register from "./components/Register/Register";
