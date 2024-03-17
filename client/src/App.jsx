d1d9a9ac4e6bb16318c6b8589c5bcf3ab4ed92a2
gabrielaDF <defreitasgabi@gmail.com>
2024-03-17 11:40:09 -0500

    agregar estilos basico a app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,22 +2,26 @@ import { useState } from "react";
 
 import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./components/Home/Home";
-import "./App.css";
+
 import Dashboard from "./components/Dashboard/Dashboard";
 import Register from "./components/Register/Register";
 import LogIn from "./components/LogIn/LogIn";
 import NotFound from "./components/NotFound/NotFound";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Navbar />
+      <div className="bg-gradient-to-r from-purpleBlue1 via-darkPurple to-darkBlue h-screen flex justify-center items-center text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </>
   );
 }
